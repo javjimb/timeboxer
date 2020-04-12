352d4c76e06707920232c06acaa87faa1e643bb9
julia.natsuko@gmail.com <julia.natsuko@gmail.com>
2020-04-12 15:58:10 +0200

    Add form elements ft AddTask component

--- client/src/components/tasks/AddTask.jsx
+++ client/src/components/tasks/AddTask.jsx
@@ -1,6 +1,30 @@
 // Library
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles((theme) => ({}));
 
 export default function AddTask() {
-  return <div>Add a task</div>;
+  const classes = useStyles();
+  return (
+    <form className={classes.root} noValidate autoComplete='off'>
+      <div>
+        <TextField
+          id='taskName'
+          label='Task'
+          placeholder='Enter task name'
+          variant='outlined'
+        />
+        <TextField
+          id='duration'
+          label='Duration'
+          placeholder='Enter duration'
+          variant='outlined'
+        />
+        <Button variant='contained'>+</Button>
+      </div>
+    </form>
+  );
 }
