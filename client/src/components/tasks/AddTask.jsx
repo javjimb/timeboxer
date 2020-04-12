7a71d53ba262bfcc322ca77c0f6c933307d7e1fe
julia.natsuko@gmail.com <julia.natsuko@gmail.com>
2020-04-12 16:25:24 +0200

    Style form in AddTask

--- client/src/components/tasks/AddTask.jsx
+++ client/src/components/tasks/AddTask.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
 
 const useStyles = makeStyles((theme) => ({}));
 
@@ -10,21 +11,33 @@ export default function AddTask() {
   const classes = useStyles();
   return (
     <form className={classes.root} noValidate autoComplete='off'>
-      <div>
-        <TextField
-          id='taskName'
-          label='Task'
-          placeholder='Enter task name'
-          variant='outlined'
-        />
-        <TextField
-          id='duration'
-          label='Duration'
-          placeholder='Enter duration'
-          variant='outlined'
-        />
-        <Button variant='contained'>+</Button>
-      </div>
+      <Grid container spacing={1}>
+        <Grid item xs={7}>
+          <TextField
+            id='taskName'
+            label='Task'
+            placeholder='Enter task name'
+            variant='outlined'
+            size='small'
+            fullWidth
+          />
+        </Grid>
+        <Grid item xs={3}>
+          <TextField
+            id='duration'
+            label='Duration'
+            placeholder='Enter duration'
+            variant='outlined'
+            size='small'
+            fullWidth
+          />
+        </Grid>
+        <Grid item xs={2}>
+          <Button variant='contained' fullWidth>
+            +
+          </Button>
+        </Grid>
+      </Grid>
     </form>
   );
 }
