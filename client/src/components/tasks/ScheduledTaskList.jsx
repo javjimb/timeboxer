794a990e3b801e25c9bd9d75580f61d22d062d39
julia.natsuko@gmail.com <julia.natsuko@gmail.com>
2020-04-25 12:51:40 +0200

    Change ScheduledTaskList to list element

--- client/src/components/tasks/ScheduledTaskList.jsx
+++ client/src/components/tasks/ScheduledTaskList.jsx
@@ -1,55 +1,61 @@
 // Library
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Divider from '@material-ui/core/Divider';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import IconButton from '@material-ui/core/IconButton';
+import CheckCircleOutlinedIcon from '@material-ui/icons/CheckCircleOutlined';
+import HighlightOffIcon from '@material-ui/icons/HighlightOff';
+import Tooltip from '@material-ui/core/Tooltip';
 
-const useStyles = makeStyles({
-  table: {
-    minWidth: 350,
-  },
-});
+// Components
+import ScheduledTime from './ScheduledTime';
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
+const _ = require('lodash');
 
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
-
-export default function ScheduledTaskList() {
-  const classes = useStyles();
+export default function ScheduledTaskList({ taskList }) {
+  //   only show if there are scheduled tasks
+  let scheduledTime = _.some(taskList, { status: 'scheduled' }) ? (
+    <scheduledTime taskList={taskList} />
+  ) : (
+    ''
+  );
 
   return (
-    <TableContainer component={Paper}>
-      <Table className={classes.table} aria-label='simple table'>
-        {/* <TableHead>
-          <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align='right'>Calories</TableCell>
-          </TableRow>
-        </TableHead> */}
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow key={row.name}>
-              <TableCell component='th' scope='row'>
-                {row.name}
-              </TableCell>
-              <TableCell align='right'>{row.calories}</TableCell>
-            </TableRow>
+    <div>
+      <List>
+        {scheduledTime}
+        {taskList
+          .filter((item) => item.status == 'scheduled')
+          .map((task) => (
+            <ListItem
+              name={task.name}
+              duration={task.duration}
+              id={task._id}
+              key={task._id}>
+              <ListItemText primary={task.name + ' ' + task.duration + 'hrs'} />
+              <ListItemSecondaryAction>
+                <Tooltip title='Mark as completed'>
+                  <IconButton
+                    edge='end'
+                    aria-label='completed'
+                    //   onClick={() => deleteTask(task._id)}
+                  >
+                    <CheckCircleOutlinedIcon />
+                  </IconButton>
+                </Tooltip>
+                <Tooltip title='Removed from scheduled'>
+                  <IconButton edge='end'>
+                    <HighlightOffIcon />
+                  </IconButton>
+                </Tooltip>
+              </ListItemSecondaryAction>
+            </ListItem>
           ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+        <Divider />
+      </List>
+    </div>
   );
 }
