6c96f9156ad448f45020d0356e149a269c094f32
Javier <javjimb@gmail.com>
2020-05-10 11:04:37 +0200

    Add prevous, next and today buttons to app toolbar

--- client/src/components/TBAppBar.jsx
+++ client/src/components/TBAppBar.jsx
@@ -5,6 +5,9 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import QueryBuilderIcon from '@material-ui/icons/QueryBuilder';
+import NavigateNextIcon from '@material-ui/icons/NavigateNext';
+import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore';
+import TodayIcon from '@material-ui/icons/Today';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -18,7 +21,7 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function TBAppBar() {
+export default function TBAppBar({next, prev, today}) {
   const classes = useStyles();
 
   return (
@@ -35,6 +38,15 @@ export default function TBAppBar() {
           <Typography variant='h6' className={classes.title}>
             Timeboxing App
           </Typography>
+            <IconButton color="inherit" onClick={prev}>
+              <NavigateBeforeIcon />
+            </IconButton>
+            <IconButton  color="inherit" onClick={today}>
+              <TodayIcon />
+            </IconButton>
+            <IconButton  color="inherit" onClick={next}>
+              <NavigateNextIcon />
+            </IconButton>
         </Toolbar>
       </AppBar>
     </div>
