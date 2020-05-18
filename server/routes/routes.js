959a4857f54b1ab7a6c363db544fbc2b1de4a23f
Javier <javjimb@gmail.com>
2020-05-18 15:00:24 +0200

    Create POST user route with tests

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const TasksController = require('../controllers/tasks.controller');
+const UsersController = require('../controllers/users.controller');
 const { check } = require('express-validator');
 
 const router = new express.Router();
@@ -204,4 +205,52 @@ router.put('/tasks/:id', [
  */
 router.delete('/tasks/:id', TasksController.delete);
 
+router.post('/users', UsersController.create);
+
+/**
+ * @api {post} /users Create a user
+ * @apiGroup User
+ * @apiParam {String} email User email
+ * @apiParam {String} password User password
+ * @apiParam {String} [name] User name
+ * @apiParam {String} [surname] User syrname
+ *
+ * @apiSuccess {String} _id User id
+ * @apiSuccess {String} name User name
+ * @apiSuccess {String} surname User surname
+ * @apiSuccess {String} email User email
+ *
+ * @apiSuccess {Date} updatedAt When the last update was made
+ * @apiSuccess {Date} createdAt When the user was created
+ * @apiSuccessExample {json} Success
+ *    HTTP/1.1 201 Created
+ *     {
+ *     _id: '5ec27dc7cf8f3b536a3be300',
+ *       name: 'Keara',
+ *       surname: 'Muller',
+ *       email: 'Gilberto19@hotmail.com',
+ *       createdAt: '2020-05-18T12:21:27.018Z',
+ *       updatedAt: '2020-05-18T12:21:27.018Z',
+ *     }
+ *
+ * @apiErrorExample {json} Validation
+ *    HTTP/1.1 422 Unprocessable Entity
+ *    {
+ *      "errors": [
+ *         {
+ *           "msg": "Email is required ",
+ *           "param": "email",
+ *           "location": "body"
+ *         },
+ *         {
+ *           "value": "123",
+ *           "msg": "Password must be at least 6 characters long",
+ *           "param": "password",
+ *           "location": "body"
+ *         }
+ *       ]
+ *     }
+ * @apiErrorExample {json} Server
+ *    HTTP/1.1 500 Internal Server Error
+ */
 module.exports = router;
