542e6a5ad0560454d0739bf3d6b75f6d8188d3f1
julia.natsuko@gmail.com <julia.natsuko@gmail.com>
2020-05-20 15:42:07 +0200

    Merge stash

--- client/src/pages/Main.jsx
+++ client/src/pages/Main.jsx
@@ -11,7 +11,7 @@ import TaskService from '../services/TaskService';
 import Scheduler from '../components/schedule/Scheduler';
 import AddTask from '../components/tasks/AddTask';
 import ScheduledTaskList from '../components/tasks/ScheduledTaskList';
-import TBAppBar from "../components/TBAppBar";
+import TBAppBar from '../components/TBAppBar';
 
 const _ = require('lodash');
 const moment = require('moment');
@@ -34,34 +34,38 @@ export default function Main() {
   const [duration, setDuration] = useState('');
   const [taskList, setTaskList] = useState([]);
   const [newTaskList, setNewTaskList] = useState([]);
-  const [startTimestamp, setStartTimestamp] = useState(moment().startOf('day').unix());
-  const [endTimestamp, setEndTimestamp] = useState(moment().endOf('day').unix());
+  const [startTimestamp, setStartTimestamp] = useState(
+    moment().startOf('day').unix()
+  );
+  const [endTimestamp, setEndTimestamp] = useState(
+    moment().endOf('day').unix()
+  );
 
-  const schedulerRef = React.createRef()
+  const schedulerRef = React.createRef();
 
   /**
    * Callback when the scheduler dates are changed
-  */
+   */
   const onDateChange = (data) => {
     setStartTimestamp(moment(data.view.activeStart).unix());
     setEndTimestamp(moment(data.view.activeEnd).unix());
     console.log('Date has changed: ', startTimestamp, endTimestamp);
-  }
+  };
 
   const goToNext = () => {
     let scheduler = schedulerRef.current;
     scheduler.next();
-  }
+  };
 
   const goToPrevious = () => {
     let scheduler = schedulerRef.current;
     scheduler.previous();
-  }
+  };
 
   const goToToday = () => {
     let scheduler = schedulerRef.current;
     scheduler.today();
-  }
+  };
 
   const taskChangeHandler = (event) => {
     setTask(event.target.value);
@@ -100,118 +104,115 @@ export default function Main() {
   const updateTask = (id, newData) => {
     // update the task in the database
     TaskService.updateTask(id, newData)
-        .then((updatedTask) => {
-
-          setTaskList(taskList.map(task => (task._id === id ? _.extend(updatedTask) : task)))
-          setNewTaskList(newTaskList.map(task => (task._id === id ? _.extend(updatedTask) : task)))
-
-        }).catch( err => {
-          // TODO: create a global error handler
-          console.error(err);
-    });
+      .then((updatedTask) => {
+        setTaskList(
+          taskList.map((task) =>
+            task._id === id ? _.extend(updatedTask) : task
+          )
+        );
+        setNewTaskList(
+          newTaskList.map((task) =>
+            task._id === id ? _.extend(updatedTask) : task
+          )
+        );
+      })
+      .catch((err) => {
+        // TODO: create a global error handler
+        console.error(err);
+      });
   };
 
   const updateTaskStatus = (id, oldStatus, newStatus) => {
-
     // update the task in the database
-    TaskService.updateTask(id, {status: newStatus})
-        .then((updatedTask) => {
-
-          // Update our lists in the state to react to the new status
-          switch (oldStatus) {
-            case 'scheduled':
-                if (newStatus === 'completed') {
-                  setTaskList(taskList.map(task => (task._id === id ? _.extend(updatedTask) : task)));
-                } else if (newStatus === 'new') {
-                  TaskService.updateTask(id, {start: 0, end: 0}).then(response => {
-                    setNewTaskList([...newTaskList, response]);
-                    setTaskList(taskList.filter( task => task._id !== id));
-                  });
+    TaskService.updateTask(id, { status: newStatus })
+      .then((updatedTask) => {
+        // Update our lists in the state to react to the new status
+        switch (oldStatus) {
+          case 'scheduled':
+            if (newStatus === 'completed') {
+              setTaskList(
+                taskList.map((task) =>
+                  task._id === id ? _.extend(updatedTask) : task
+                )
+              );
+            } else if (newStatus === 'new') {
+              TaskService.updateTask(id, { start: 0, end: 0 }).then(
+                (response) => {
+                  setNewTaskList([...newTaskList, response]);
+                  setTaskList(taskList.filter((task) => task._id !== id));
                 }
-              break;
-            case 'new':
-              setNewTaskList(newTaskList.filter( task => task._id !== id));
-              setTaskList([...taskList, updatedTask]);
-              break;
-            case 'completed':
-              setNewTaskList([...newTaskList, updatedTask]);
-              setTaskList(taskList.filter( task => task._id !== id));
-              break;
-          }
-
-        }).catch( err => {
-          // TODO: create a global error handler
-          console.error(err);
-    });
+              );
+            }
+            break;
+          case 'new':
+            setNewTaskList(newTaskList.filter((task) => task._id !== id));
+            setTaskList([...taskList, updatedTask]);
+            break;
+          case 'completed':
+            setNewTaskList([...newTaskList, updatedTask]);
+            setTaskList(taskList.filter((task) => task._id !== id));
+            break;
+        }
+      })
+      .catch((err) => {
+        // TODO: create a global error handler
+        console.error(err);
+      });
   };
 
-  let fromToday = moment().startOf('day').unix();
-
   useEffect(() => {
+    let fromToday = moment().startOf('day').unix();
+    TaskService.getAllTasks({ fromTimestamp: fromToday })
+      .then((response) => {
+        response.json();
+      })
+      .then((response) => setTaskList(response.tasks))
+      .catch((error) => console.error(error));
 
-    async function fetchTasks() {
-      const response = await fetch(
-        `http://localhost:5000/tasks?fromTimestamp=${fromToday}`
-      );
-      response
-        .json()
-        .then((response) => setTaskList(response.tasks))
-        .catch((error) => console.error(error));
-    }
-
-    async function fetchNewTasks() {
-      const response = await fetch(
-        `http://localhost:5000/tasks?status=new`
-      );
-      response
-        .json()
-        .then((response) => setNewTaskList(response.tasks))
-        .catch((error) => console.error(error));
-    }
-
-    fetchTasks();
-    fetchNewTasks();
-
+    TaskService.getAllTasks({ status: 'new' })
+      .then((response) => {
+        response.json();
+      })
+      .then((response) => setNewTaskList(response.tasks))
+      .catch((error) => console.error(error));
   }, []);
 
-
-
   return (
-      <div>
-        <TBAppBar next={goToNext}  prev={goToPrevious} today={goToToday} />
-        <div id='task-list' className={classes.root}>
-          <Grid container spacing={0}>
-            <Grid item xs={12} sm={4}>
-              <Paper className={classes.paper}>
-                <AddTask
-                    task={task}
-                    duration={duration}
-                    createNewTask={createNewTask}
-                    taskChangeHandler={taskChangeHandler}
-                    durationChangeHandler={durationChangeHandler}
-                    taskList={newTaskList}
-                    deleteTask={deleteTask}
-                />
-                <ScheduledTaskList
-                  taskList={taskList}
-                  updateTaskStatus={updateTaskStatus}
-                />
-              </Paper>
-              {/* <Paper className={classes.paper}></Paper> */}
-            </Grid>
-            <Grid item xs={12} sm={8}>
-              <Paper className={classes.paper}>
-                <Scheduler
-                  taskList={taskList}
-                  updateTask={updateTask}
-                  updateTaskStatus={updateTaskStatus}
-                  onDateChange={(start,end) => onDateChange(start,end)}
-                  ref={schedulerRef}
-                />
-              </Paper>
-            </Grid>
+    <div>
+      <TBAppBar next={goToNext} prev={goToPrevious} today={goToToday} />
+      <div id='task-list' className={classes.root}>
+        <Grid container spacing={0}>
+          <Grid item xs={12} sm={4}>
+            <Paper className={classes.paper}>
+              <AddTask
+                task={task}
+                duration={duration}
+                createNewTask={createNewTask}
+                taskChangeHandler={taskChangeHandler}
+                durationChangeHandler={durationChangeHandler}
+                taskList={newTaskList}
+                deleteTask={deleteTask}
+              />
+              <ScheduledTaskList
+                taskList={taskList}
+                updateTaskStatus={updateTaskStatus}
+              />
+            </Paper>
+            {/* <Paper className={classes.paper}></Paper> */}
           </Grid>
-        </div>
+          <Grid item xs={12} sm={8}>
+            <Paper className={classes.paper}>
+              <Scheduler
+                taskList={taskList}
+                updateTask={updateTask}
+                updateTaskStatus={updateTaskStatus}
+                onDateChange={(start, end) => onDateChange(start, end)}
+                ref={schedulerRef}
+              />
+            </Paper>
+          </Grid>
+        </Grid>
       </div>
+    </div>
   );
 }
--- client/src/services/TaskService.js
+++ client/src/services/TaskService.js
@@ -1,6 +1,8 @@
 const apiURL = 'http://localhost:5000/tasks/';
+const querystring = require('querystring');
 
 export default {
+<<<<<<< Updated upstream
     /**
      * Updates a task
      * @param task_id string unique id of the task
@@ -24,4 +26,30 @@ export default {
         const data = await response.json();
         return data;
     },
+=======
+  /**
+   * Updates a task
+   * @param task_id string unique id of the task
+   * @param newData object of new data for the task
+   * @returns {Promise<any>}
+   */
+  async updateTask(task_id, newData) {
+    const requestOptions = {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(newData),
+    };
+    const response = await fetch(apiURL + task_id, requestOptions);
+    const data = await response.json();
+
+    return data;
+  },
+  async getAllTasks(params) {
+    let queryString = querystring.stringify(params);
+    const response = await fetch(apiURL + '?' + queryString);
+    const data = await response.json();
+
+    return data;
+  },
+>>>>>>> Stashed changes
 };
