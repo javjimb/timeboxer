bf9d920ddb588cfbed5a4a37ca614bf9b1044285
julia.natsuko@gmail.com <julia.natsuko@gmail.com>
2020-05-20 17:16:10 +0200

    Add a service to delete a task

--- client/src/services/TaskService.js
+++ client/src/services/TaskService.js
@@ -19,11 +19,31 @@ export default {
 
         return data;
     },
+    /**
+     * Gets all tasks
+     * @param params string or number for query
+     * @returns {Promise<any>}
+     */
     async getAllTasks(params) {
         let queryString = querystring.stringify(params);
         const response = await fetch(apiURL + '?' + queryString);
         const data = await response.json();
 
+        return data;
+    },
+
+    /**
+     * Deletes a task
+     * @param task_id string unique id of the task
+     * @returns {Promise<any>}
+     */
+    async deleteTask(task_id) {
+        const requestOptions = {
+            method: 'DELETE',
+            headers: { 'Content-Type': 'application/json' },
+        };
+        const response = await fetch(apiURL + task_id, requestOptions);
+        const data = await response.json();
         return data;
     },
 };
