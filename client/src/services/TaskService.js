42eea33a860100bf3fdf01535d14cb1191b6a624
julia.natsuko@gmail.com <julia.natsuko@gmail.com>
2020-05-20 15:37:14 +0200

    Add getTasks

--- client/src/services/TaskService.js
+++ client/src/services/TaskService.js
@@ -1,7 +1,6 @@
 const apiURL = 'http://localhost:5000/tasks/';
 
 export default {
-
     /**
      * Updates a task
      * @param task_id string unique id of the task
@@ -9,15 +8,20 @@ export default {
      * @returns {Promise<any>}
      */
     async updateTask(task_id, newData) {
-
         const requestOptions = {
             method: 'PUT',
             headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(newData)
+            body: JSON.stringify(newData),
         };
         const response = await fetch(apiURL + task_id, requestOptions);
         const data = await response.json();
 
         return data;
-    }
-}
+    },
+
+    async getTasks() {
+        const response = await fetch(apiURL);
+        const data = await response.json();
+        return data;
+    },
+};
