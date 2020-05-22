ce344148674b7cee8d79157c877145e09dbb53cf
julia.natsuko@gmail.com <julia.natsuko@gmail.com>
2020-05-22 10:01:34 +0200

    Solve conflict

--- client/src/services/TaskService.js
+++ client/src/services/TaskService.js
@@ -2,31 +2,6 @@ const apiURL = 'http://localhost:5000/tasks/';
 const querystring = require('querystring');
 
 export default {
-<<<<<<< Updated upstream
-    /**
-     * Updates a task
-     * @param task_id string unique id of the task
-     * @param newData object of new data for the task
-     * @returns {Promise<any>}
-     */
-    async updateTask(task_id, newData) {
-        const requestOptions = {
-            method: 'PUT',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(newData),
-        };
-        const response = await fetch(apiURL + task_id, requestOptions);
-        const data = await response.json();
-
-        return data;
-    },
-
-    async getTasks() {
-        const response = await fetch(apiURL);
-        const data = await response.json();
-        return data;
-    },
-=======
   /**
    * Updates a task
    * @param task_id string unique id of the task
@@ -51,5 +26,4 @@ export default {
 
     return data;
   },
->>>>>>> Stashed changes
 };
