e509f5d44e354ba35a22cb6bbfe2bc6a629c23a4
julia.natsuko@gmail.com <julia.natsuko@gmail.com>
2020-06-17 11:36:34 +0200

    Add AppBar to signIn and Up pages, change button color

---
+++ client/src/components/SignAppBar.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        flexGrow: 1,
+    },
+}));
+
+export default function TBAppBar({ next, prev, today }) {
+    const classes = useStyles();
+
+    return (
+        <div className={classes.root}>
+            <AppBar position="static" style={{ backgroundColor: "#3788d8" }}>
+                <Toolbar>
+                    <div>
+                        <img
+                            src="https://res.cloudinary.com/dcmfiobqe/image/upload/v1590138327/timeboxer_white_p74w3f.png"
+                            alt="logo"
+                            style={{ height: "80px", padding: "3px" }}
+                        />
+                    </div>
+                    <Typography variant="h2" className={classes.title}>
+                        Time Boxer
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+        </div>
+    );
+}
