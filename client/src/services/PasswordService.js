9ac612516413d226f93698d60087b7df290a3752
julia.natsuko@gmail.com <julia.natsuko@gmail.com>
2020-07-15 13:11:29 +0200

    Add password page and rout with POST request

---
+++ client/src/services/PasswordService.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+const apiURL = process.env.REACT_APP_API_URL + "/password";
+
+export default {
+    async resetPassword(email) {
+        const requestOptions = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                email: email,
+            }),
+        };
+        const response = await fetch(apiURL, requestOptions);
+
+        if (response.status === 204) {
+            return true;
+        } else {
+            response.reject(response.json.errors);
+        }
+    },
+};
