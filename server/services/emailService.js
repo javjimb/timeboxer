692f731d6a700f123be418367614617ab54c4c05
Javier <javjimb@gmail.com>
2020-07-15 09:24:38 +0200

    Create reset password API endpoint

--- server/services/emailService.js
+++ server/services/emailService.js
@@ -30,9 +30,32 @@ class emailService {
                 console.log(err);
             } else {
                 that.transporter.sendMail({
-                    from: '"Timeboxer" <no-reply@time-boxer.com>',
+                    from: '"Time-boxer" <no-reply@time-boxer.com>',
                     to: user.email,
-                    subject: "Timeboxer: Verify your email",
+                    subject: "Time-boxer: Verify your email",
+                    html: html,
+                }).then(info => {
+                    //console.log("Message sent: %s", info.messageId);
+                });
+            }
+        });
+    }
+
+    sendResetPasswordEmail(user, password) {
+        // do not send if in test mode
+        if (process.env.NODE_ENV === 'test') {
+            return;
+        }
+
+        let that = this;
+        ejs.renderFile('./emails/' + "resetPassword.ejs", { user: user, password: password, appUrl: this.appUrl }, function (err, html) {
+            if (err) {
+                console.log(err);
+            } else {
+                that.transporter.sendMail({
+                    from: '"Time-boxer" <info@time-boxer.com>',
+                    to: user.email,
+                    subject: "Time-boxer: Verify your email",
                     html: html,
                 }).then(info => {
                     //console.log("Message sent: %s", info.messageId);
