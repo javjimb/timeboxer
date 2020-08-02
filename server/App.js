ed22ae6ac96d51b4bc2e837f6901941bb732b2c5
Javier <javjimb@gmail.com>
2020-08-02 13:21:42 +0200

    API supports development and production environments

--- server/App.js
+++ server/App.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config({ path: `./config/${process.env.NODE_ENV}/.env`});
 const express = require('express');
 const cors = require('cors');
 const databaseHelper = require('./helpers/database');
