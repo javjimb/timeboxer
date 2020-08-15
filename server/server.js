b5e924e7e2a1cfc00f5853675c7f5d420ea80818
Javier <javjimb@gmail.com>
2020-08-15 18:01:18 +0200

    Add SSL certificate config to example .env files

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const app = require("./App");
-
+const https = require("https");
+const fs = require("fs");
 
 app.use(function(req, res, next) {
     res.status(404);
@@ -9,3 +10,14 @@ app.use(function(req, res, next) {
 app.listen(process.env.NODE_PORT, () => {
     console.log('App running on port ' + process.env.NODE_PORT);
 });
+
+/*
+const options = {
+  key: fs.readFileSync(process.env.SSL_KEY),
+  cert: fs.readFileSync(process.env.SSL_CERT)
+}
+
+https.createServer(options, app).listen(process.env.NODE_PORT, () => {
+    console.log('App running on port ' + process.env.NODE_PORT);
+});
+ */
