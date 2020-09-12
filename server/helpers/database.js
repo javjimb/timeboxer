13303fb4b991c72c8f742dad0a02d853a43f02f0
Javier <javjimb@gmail.com>
2020-09-12 13:48:32 +0200

    Connect to database using authentication

--- server/helpers/database.js
+++ server/helpers/database.js
@@ -2,9 +2,13 @@ const mongoose = require('mongoose');
 
 const connect = async () => {
     if (mongoose.connection.readyState === 0) {
+        let DB_URI = 'mongodb://' + process.env.MONGODB_HOST + ':' + process.env.MONGODB_PORT + '/' + process.env.MONGODB_DATABASE;
         await mongoose.connect(
-            process.env.NODE_ENV === 'test' ? global.__DB_URL__ : process.env.DB_URL,
+            process.env.NODE_ENV === 'test' ? global.__DB_URL__ : DB_URI,
             {
+                authSource: 'admin',
+                user: process.env.MONGODB_USERNAME,
+                pass: process.env.MONGODB_PASSWORD,
                 useNewUrlParser: true,
                 useCreateIndex: true,
                 useFindAndModify: false,
