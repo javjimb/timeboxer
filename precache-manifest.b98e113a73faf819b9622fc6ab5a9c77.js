c2a82e040e6c9f3f1ebc92f0ac8a2bb25d7b39da
Javier Jimenez <javjimb@gmail.com>
2020-09-14 21:22:35 +0200

    Updates

---
+++ precache-manifest.b98e113a73faf819b9622fc6ab5a9c77.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a4e61f3d1b517ac7d065027b1eb4999",
+    "url": "/timeboxer/index.html"
+  },
+  {
+    "revision": "31da42496e3e3cc61e24",
+    "url": "/timeboxer/static/css/2.577802f3.chunk.css"
+  },
+  {
+    "revision": "84ca82964ae6100897ea",
+    "url": "/timeboxer/static/css/main.a8606e58.chunk.css"
+  },
+  {
+    "revision": "31da42496e3e3cc61e24",
+    "url": "/timeboxer/static/js/2.141089ad.chunk.js"
+  },
+  {
+    "revision": "e1f93d326bbd51cb0c0d4f43c326965a",
+    "url": "/timeboxer/static/js/2.141089ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84ca82964ae6100897ea",
+    "url": "/timeboxer/static/js/main.31ab8061.chunk.js"
+  },
+  {
+    "revision": "921ad8402ab5e4d86b70",
+    "url": "/timeboxer/static/js/runtime-main.e8086e33.js"
+  },
+  {
+    "revision": "061ec4e6d3dbbc50c552779b337a7ecb",
+    "url": "/timeboxer/static/media/time-head.061ec4e6.jpg"
+  }
+]);
