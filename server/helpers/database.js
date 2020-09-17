13442fd81acdf578face2406ad1669ef67529b35
Javier <javjimb@gmail.com>
2020-09-17 17:56:57 +0200

    Add publishheroku script

--- server/helpers/database.js
+++ server/helpers/database.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const connect = async () => {
     if (mongoose.connection.readyState === 0) {
         let DB_URI = 'mongodb://' + process.env.MONGODB_HOST + ':' + process.env.MONGODB_PORT + '/' + process.env.MONGODB_DATABASE;
+        console.log(DB_URI);
         await mongoose.connect(
             process.env.NODE_ENV === 'test' ? global.__DB_URL__ : DB_URI,
             {
